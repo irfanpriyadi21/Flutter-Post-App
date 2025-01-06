01107c933ad299556f5ce6bb7b7e8277f01bb5be
irfanpriyadi21 <whantz1004@gmail.com>
2025-01-06 11:18:15 +0700

    update

---
+++ lib/data/datasources/product_remote_datasource.dart
@@ -0,0 +1,29 @@
+
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
+import 'package:mobile_pos_app/core/constants/variables.dart';
+import 'package:mobile_pos_app/data/datasources/auth_local_datasource.dart';
+import 'package:mobile_pos_app/data/models/response/product_response_model.dart';
+import 'package:http/http.dart' as http;
+
+
+class ProductRemoteDatasource {
+
+  Future<Either<String, ProductResponseModel>> getProduct()async{
+    final authData = await AuthLocalDatasource().getAuthData();
+    final response = await http.get(
+      Uri.parse("${Variables.baseUrl}/api/products"),
+       headers: {
+        "Authorization" : "Bearer ${authData.token}"
+      }
+    );
+    if(response.statusCode == 200){
+      final responseData = json.decode(response.body);
+      return right(ProductResponseModel.fromRawJson(responseData));
+    }else{
+      return left(response.body);
+    }
+    
+  }
+}
---
+++ lib/data/models/response/product_response_model.dart
@@ -0,0 +1,106 @@
+import 'package:meta/meta.dart';
+import 'dart:convert';
+
+class ProductResponseModel {
+    final bool success;
+    final String message;
+    final List<Product> data;
+
+    ProductResponseModel({
+        required this.success,
+        required this.message,
+        required this.data,
+    });
+
+    factory ProductResponseModel.fromRawJson(String str) => ProductResponseModel.fromJson(json.decode(str));
+
+    String toRawJson() => json.encode(toJson());
+
+    factory ProductResponseModel.fromJson(Map<String, dynamic> json) => ProductResponseModel(
+        success: json["success"],
+        message: json["message"],
+        data: List<Product>.from(json["data"].map((x) => Product.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "success": success,
+        "message": message,
+        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+    };
+}
+
+class Product {
+    final int id;
+    final String name;
+    final String description;
+    final int price;
+    final int stock;
+    final Category category;
+    final String image;
+    final DateTime createdAt;
+    final DateTime updatedAt;
+
+    Product({
+        required this.id,
+        required this.name,
+        required this.description,
+        required this.price,
+        required this.stock,
+        required this.category,
+        required this.image,
+        required this.createdAt,
+        required this.updatedAt,
+    });
+
+    factory Product.fromRawJson(String str) => Product.fromJson(json.decode(str));
+
+    String toRawJson() => json.encode(toJson());
+
+    factory Product.fromJson(Map<String, dynamic> json) => Product(
+        id: json["id"],
+        name: json["name"],
+        description: json["description"],
+        price: json["price"],
+        stock: json["stock"],
+        category: categoryValues.map[json["category"]]!,
+        image: json["image"],
+        createdAt: DateTime.parse(json["created_at"]),
+        updatedAt: DateTime.parse(json["updated_at"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "description": description,
+        "price": price,
+        "stock": stock,
+        "category": categoryValues.reverse[category],
+        "image": image,
+        "created_at": createdAt.toIso8601String(),
+        "updated_at": updatedAt.toIso8601String(),
+    };
+}
+
+enum Category {
+    DRINK,
+    FOOD,
+    SNACK
+}
+
+final categoryValues = EnumValues({
+    "drink": Category.DRINK,
+    "food": Category.FOOD,
+    "snack": Category.SNACK
+});
+
+class EnumValues<T> {
+    Map<String, T> map;
+    late Map<T, String> reverseMap;
+
+    EnumValues(this.map);
+
+    Map<T, String> get reverse {
+            reverseMap = map.map((k, v) => MapEntry(v, k));
+            return reverseMap;
+    }
+}
